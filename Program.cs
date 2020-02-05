d00724488f6301adad6d58e6a7751d2838a24ff2
Reginald Beninche <rbeninche@gmail.com>
2020-02-04 23:20:59 -0500

    Version 2.0

--- Program.cs
+++ Program.cs
@@ -8,42 +8,94 @@ namespace LiveCodingFinal
 {
     class Program
     {
-
-     
-
         static void Main(string[] args)
         {
-      
+            var weatherTypes = new [] { "clear", "cloudy", "Raining", "Full Moon" };
 
-            double averageCandy = 250;
+            var temperature = new [] { "40", "50", "60", "70" };
 
-            double averageKids = 3;
+            double averageKids = 250;
 
-            double percentage = 0;
+            double candyPerKids = 3;
 
-            double moreKids = averageKids + (percentage + averageKids);
+            double weatherPercentage;
+
+            double temperaturePercentage;
+
+
+
+            double averagePercentage;
+
+            double totalCandy;
 
            
 
-          
-         
+           
+            for(int i = 0; i< weatherTypes.Length; i++)
+            {
 
+                for (int j = 0; j < temperature.Length; j++)
+                {
+                    if (weatherTypes[i] == "clear" && temperature[j] == "50")
+                    {
+                        
+                        weatherPercentage = 0.10;
+                        temperaturePercentage = 0;
+                        averagePercentage = ((averageKids * weatherPercentage) + (averageKids * temperaturePercentage)) + averageKids;
+                        totalCandy = averagePercentage * candyPerKids;
+
+                        
+
+                        Console.WriteLine(totalCandy);
+
+                    }
+                   
+                    if (weatherTypes[i] == "Raining" && temperature[j] == "70")
+                    {
+                        weatherPercentage = -0.25;
+                        temperaturePercentage = 0.20;
+                        averagePercentage = ((averageKids * weatherPercentage) + (averageKids * temperaturePercentage)) + averageKids;
+                        totalCandy = averagePercentage * candyPerKids;
+                        Console.WriteLine(totalCandy);
+
+                    }
+                    if (weatherTypes[i] == "Full Moon" && temperature[j] == "40")
+                    {
+                        weatherPercentage = 0.25;
+                        temperaturePercentage = -0.05;
+                        averagePercentage = ((averageKids * weatherPercentage) + (averageKids * temperaturePercentage)) + averageKids;
+                        totalCandy = averagePercentage * candyPerKids;
+                        Console.WriteLine(totalCandy);
+
+                    }
+
+
+                }
+               
+            }
+           
+
+            Console.ReadLine();
+
+
+           
+
+            
+ 
             
 
         }
 
-
-        static void Tricks(string weatherTypes, int temperature)
-        {
+     
 
 
-            if(weatherTypes == "Clear")
-            {
-                
-            }
 
-        }
 
-      
     }
+
+    
+
+
+
+    
 }
